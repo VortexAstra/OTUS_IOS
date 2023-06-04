85344a5293c5ebb75a04a74b230fe291f63e7c6f
Artyom Ustinov <artem.ustinov@corp.mail.ru>
2023-06-04 22:10:14 +0300

    Init commit

---
+++ OTUS_1/OTUS_1/OTUS_1/OTUS_1App.swift
@@ -0,0 +1,17 @@
+//
+//  OTUS_1App.swift
+//  OTUS_1
+//
+//  Created by artem-ustinov on 04.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct OTUS_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
